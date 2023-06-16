2811f102f52934f2b3778a8d36c8d7c2a86df07b
themayursaini <mayursainiofficial@gmail.com>
2023-06-16 14:05:23 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar(props) {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           {/* <Link className="nav-link active" aria-current="page" to="TextForm">Text Form</Link> */}
-          <a className="nav-link active" aria-current="page" href="TextForm">Text Form</a>
+          <a className="nav-link active" aria-current="page" href="">Text Form</a>
         </li>
 
         <li className="nav-item22">
